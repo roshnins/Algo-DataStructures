79318a938cc3033ff55345d4fa7d2d32799c07d8
roshnins <roshnins@hotmail.com>
2022-10-10 15:52:42 +0800

    initial commit

---
+++ misc/classes/Cookie.java
@@ -0,0 +1,18 @@
+package misc.classes;
+
+public class Cookie {
+    private String color;
+
+    public Cookie(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+}
